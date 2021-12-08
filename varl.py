b3ac457eec3d02789f870423b435e5d6ab14d60e
V-1 <chabster98@gmail.com>
2021-12-08 20:12:33 +0300

    add varl.py

---
+++ varl.py
@@ -0,0 +1,12 @@
+def get_vat(payment, persent=20):
+	try:
+
+		payment=float(payment)
+		vat=payment / 100 * persent 
+		vat=round(vat, 20)
+		return "Sum vat: {}",format(vat)
+	except TypeError:
+	     return "Can't count. Check data!"
+
+result=get_vat(400, 15)
+print(result)
